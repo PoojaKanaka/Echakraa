6393bdc5125e9fc347ba7f7be7be588083ff04f5
Shweta Ranpise Kanaka <shweta.ranpise@kanakasoftware.com>
2023-09-25 21:52:37 +0530

    Basic changes for Appointment booking

---
+++ Chakraa/Helper/AppointmentBookingHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Chakraa.Helper
+{
+    public static class City
+    {
+        public static SelectList CityDropdownOptions()
+        {
+            var cities = new List<string>() { "Mumbai", "Pune", "Delhi", "Banglore", "Hydrabad", "Indore" };
+            return new SelectList(cities, cities[0]);
+        }
+    }
+    public static class Departments
+    {
+        public static SelectList DepartmentDropdownOptions()
+        {
+            var departments = new List<string>() { "Dentist", "Gynecologist", "General Physician", "Dermatologist",
+                "ENT Specialist" };
+            return new SelectList(departments, departments[0]);
+        }
+    }
+}
---
+++ Chakraa/Models/AppointmentBookingModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Chakraa.Models
+{
+    public class AppointmentBookingModel
+    {
+        public int CityID { get; set; }
+        public string CityName { get; set; }
+        public int DepartmentID { get; set; }
+        public string DepartmentName { get; set; }
+
+    }
+
+}
